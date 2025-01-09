02c9a419ec4f06f9b74e6c30492a02d37085cb63
Aarush0-5 <singhaar0110@gmail.com>
2025-01-09 22:37:18 +0530

    cors update p3

--- backend/routes/podcastcrudRoutes.js
+++ backend/routes/podcastcrudRoutes.js
@@ -4,7 +4,7 @@ import {protectedRoute} from "../middleware/protectedRoute.js";
 const router = express.Router();
 
 router.get("/auth/podcast", podcastget);
-router.post("/auth/podcast",protectedRoute,  podcastpost);
+router.post("/auth/podcast", podcastpost);
 router.put("/auth/podcast/:id",protectedRoute, podcastput);
 router.delete("/auth/podcast/:id",protectedRoute, podcastdelete);
 
