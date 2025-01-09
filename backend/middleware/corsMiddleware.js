05d6be107249089bcdfaed9ba888302148eb3b8c
Aarush0-5 <singhaar0110@gmail.com>
2025-01-09 21:16:09 +0530

    CORS origin updated

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -2,7 +2,7 @@
 import cors from "cors";
 
 const corsOptions = {
-    origin: " https://sentimetre-n2m2.onrender.com", // Allow all origins. Change this to specific domains if needed.
+    origin: "https://sentimetre-frontend.onrender.com", // Allow all origins. Change this to specific domains if needed.
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials:true,
