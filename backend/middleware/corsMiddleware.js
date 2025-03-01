ddd7537586e4d10450b9f4e7be44ca3592ad4af7
Aarush0-5 <singhaar0110@gmail.com>
2025-03-01 20:48:11 +0530

    working

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -2,7 +2,7 @@
 import cors from "cors";
 
 const corsOptions = {
-    origin: "https://www.sentimetre.live", // Allow all origins. Change this to specific domains if needed.
+    origin: "http://localhost:5173", // Allow all origins. Change this to specific domains if needed.
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials:true,
