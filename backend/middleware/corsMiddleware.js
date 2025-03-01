cf34712858684210a653235a1d8a828c43b00bf5
Aarush0-5 <singhaar0110@gmail.com>
2025-03-01 22:39:48 +0530

    Podcast ui updated

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -2,7 +2,7 @@
 import cors from "cors";
 
 const corsOptions = {
-    origin: "http://localhost:5173", // Allow all origins. Change this to specific domains if needed.
+    origin: "https://www.sentimetre.live",
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials:true,
